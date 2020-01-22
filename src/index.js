64d18cd30c6d7a035af44636b36cb4e8588e42b7
dredizzle <dredizzle@live.co.uk>
2020-01-22 21:46:36 +0000

    setup complete

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<div>hello world</div>, document.getElementById('root'));
+const App = () => (
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+  <div>hello world</div>
+  // <HashRouter>
+  //   <Switch>
+  //     <Route exact path="/" component={Home} />
+    
+  //   </Switch>
+  // </HashRouter>
+)
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
 serviceWorker.unregister();
