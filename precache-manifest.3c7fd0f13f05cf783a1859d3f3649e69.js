0d6538fddd699b35a4d231b8c3dc6b4b6c283420
dredizzle <dredizzle@live.co.uk>
2020-01-22 21:00:22 +0000

    Updates

---
+++ precache-manifest.3c7fd0f13f05cf783a1859d3f3649e69.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2177868ce55994c08c121799160f7fa",
+    "url": "/andre-urbani/index.html"
+  },
+  {
+    "revision": "14ae8ee30a063699315d",
+    "url": "/andre-urbani/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d8731753f4ed40dd7a5f",
+    "url": "/andre-urbani/static/js/2.4ed51117.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/andre-urbani/static/js/2.4ed51117.chunk.js.LICENSE"
+  },
+  {
+    "revision": "14ae8ee30a063699315d",
+    "url": "/andre-urbani/static/js/main.863165a2.chunk.js"
+  },
+  {
+    "revision": "6c04843a63291e129797",
+    "url": "/andre-urbani/static/js/runtime-main.a8bbb617.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/andre-urbani/static/media/logo.5d5d9eef.svg"
+  }
+]);
