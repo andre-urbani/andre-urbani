3d28159ba30ba5a0fafbe23f4507f7fa8b422c38
dredizzle <dredizzle@live.co.uk>
2020-01-22 21:15:33 +0000

    hello world

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import ReactDOM from 'react-dom';
+// import logo from './logo.svg'
+// import './App.css'
 
-function App() {
-  return (
-    <div>hello world</div>
-  )
-}
+const App = () => (
+
+  <div>helloooo world</div>
+  // <HashRouter>
+  //   <Switch>
+  //     <Route exact path="/" component={Home} />
+    
+  //   </Switch>
+  // </HashRouter>
+)
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 export default App
