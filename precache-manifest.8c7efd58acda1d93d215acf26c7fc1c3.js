2aa5f35cfe87ab0216a4c4df4a8d06a8a716d026
dredizzle <dredizzle@live.co.uk>
2020-01-31 10:37:03 +0000

    Updates

--- precache-manifest.8c7efd58acda1d93d215acf26c7fc1c3.js
+++ precache-manifest.8c7efd58acda1d93d215acf26c7fc1c3.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cd1bee1cbba644cd870a010f83272b45",
+    "revision": "f610cbee2c83826a390532b7c863de76",
     "url": "/andre-urbani/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/andre-urbani/static/css/2.5dabca3a.chunk.css"
   },
   {
-    "revision": "d3b6a7f0f24e2b4c3d57",
+    "revision": "a97837338a8ca276193c",
     "url": "/andre-urbani/static/css/main.e3445247.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/andre-urbani/static/js/2.ac17bbe3.chunk.js.LICENSE"
   },
   {
-    "revision": "d3b6a7f0f24e2b4c3d57",
-    "url": "/andre-urbani/static/js/main.f81feb38.chunk.js"
+    "revision": "a97837338a8ca276193c",
+    "url": "/andre-urbani/static/js/main.10021c8d.chunk.js"
   },
   {
     "revision": "6c04843a63291e129797",
